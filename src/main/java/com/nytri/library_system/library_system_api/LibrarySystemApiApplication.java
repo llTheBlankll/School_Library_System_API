a8a7857ceb67e67d33b35917d8df195f6ef2c1c3
Nytri <llTheBlankll@gmail.com>
2022-12-11 13:22:13 +0800

    Spring boot Starter

---
+++ src/main/java/com/nytri/library_system/library_system_api/LibrarySystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.nytri.library_system.library_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nytri/library_system/library_system_api/LibrarySystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nytri.library_system.library_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
