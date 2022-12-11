0ad8b15091ee9aa972ec9f142bb5abc6d876ce9d
Nytri <llTheBlankll@gmail.com>
2022-12-12 05:54:21 +0800

    Add function to search for a certain region

--- src/main/java/com/nytri/library_system/library_system_api/repository/AddressRepository.java
+++ src/main/java/com/nytri/library_system/library_system_api/repository/AddressRepository.java
@@ -4,7 +4,10 @@ import com.nytri.library_system.library_system_api.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
+    List<Address> findByRegionContains(String region);
 }
