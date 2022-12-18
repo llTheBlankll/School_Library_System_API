36921f25d79803a80893de73d9595faae01a218a
Nytri <llTheBlankll@gmail.com>
2022-12-18 13:30:15 +0800

    Spring Boot JUnit Test Created

--- src/test/java/com/nytri/library_system/library_system_api/HttpResponseTest.java
+++ src/test/java/com/nytri/library_system/library_system_api/HttpResponseTest.java
@@ -1,6 +1,6 @@
 package com.nytri.library_system.library_system_api;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,71 +9,68 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 
-
 @ContextConfiguration(classes = LibrarySystemApiApplication.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LibrarySystemApiApplicationTests {
+class HttpResponseTest {
 
     @Autowired
     private TestRestTemplate template;
-
-    private String username = "Administrator";
-    private String password = "50925092";
-
+    private final String username = "Administrator";
+    private final String password = "50925092";
 
     @Test
     public void testEmployeeController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/administrator/employees/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testAddressController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/administrator/addresses/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testStudentController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/administrator/students/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testClassroomController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/administrator/classrooms/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testAuthorController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/librarian/authors/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testBookController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/librarian/books/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testGenresController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/librarian/genres/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testBorrowerController() {
         ResponseEntity<String> response = template.withBasicAuth(username, password)
                 .getForEntity("/api/librarian/borrowers/all", String.class);
-        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
